19e19794a56073604fc059a2b0fd0184b4c1ebb5
M Sufyan Mughal <mskhalid703@gmail.com>
2025-06-13 14:23:57 +0500

    hello

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:[
-            'https://www.pexels.com/',
-            'lh3.googleusercontent.com'
-        ]
-    }
 };
 
 export default nextConfig;
